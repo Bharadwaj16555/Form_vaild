c3678f5ef100e7ff3c72a1c8afa9c4e7be006f88
Bharadwaj16555 <bharadwajrhl@gmail.com>
2025-08-25 15:29:04 +0530

    Success Message

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Form/',  // Replace with your actual repo name
+  // Replace with your actual repo name
   plugins: [react()],
 })
 
